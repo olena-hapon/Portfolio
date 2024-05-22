4ffcfe44f2abd593229447146cb3dae7ca6153a2
Olena Hapon <olenagapon90gmail.com>
2024-05-22 10:05:50 +0300

    added changes

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -11,7 +11,7 @@ const items = [
     color: 'from-red-300 to-blue-300',
     title: 'React Desire Store',
     desc: 'A single-page application implements online store functionality. User can browse products, add them to the cart and favorites, search products, sort products, change the quantity of products in the cart and view product details. Layout is adaptive and responsive as well as cross-browser compatible. It works correctly on all screen sizes (mobile, tablet and desktop versions). Technologies used: React, React Router, TypeScript, JavaScript, LoDash, Fetch, API, Sass (SCSS), BEM methodology',
-    img: '/desire4.png',
+    img: './desire4.png',
     link:'https://olena-hapon.github.io/desire-store/',
   },
   {
@@ -19,7 +19,7 @@ const items = [
     color: 'from-blue-300 to-violet-300',
     title: 'React Phone Store',
     desc: 'A single-page application implements online store functionality. User can browse products, add them to the cart and favorites, search products, sort products, change the quantity of products in the cart and view product details. Layout is adaptive and responsive as well as cross-browser compatible. It works correctly on all screen sizes (mobile, tablet and desktop versions). Technologies used: React, React Router, TypeScript, JavaScript, LoDash, Fetch, API, Sass (SCSS), BEM methodology.',
-    img: '/store4.png',
+    img: './store4.png',
     link: 'https://olena-hapon.github.io/react_phone-catalog/',
   },
   {
@@ -27,7 +27,7 @@ const items = [
     color: 'from-violet-300 to-purple-300',
     title: 'Dia',
     desc: 'Landing page for Dia store. Presents the company itself, team, and services. Describes the benefits of the company. Contains a form for sending a request for a consultation. Layout is adaptive and responsive as well as cross-browser compatible. It works correctly on all screen sizes (mobile, tablet and desktop versions). Technologies used: HTML, CSS, SCSS, JavaScript, BEM methodology.',
-    img: '/dia4.png',
+    img: './dia4.png',
     link: 'https://olena-hapon.github.io/layout_dia/',
   },
   {
@@ -35,7 +35,7 @@ const items = [
     color: 'from-purple-300 to-red-300',
     title: 'Miami',
     desc: 'Landing page for Miami company. Presents the company itself, team, and services. Describes the benefits of the company. Contains a form for sending a request for a consultation. Layout is adaptive and responsive as well as cross-browser compatible. It works correctly on all screen sizes (mobile, tablet and desktop versions). Technologies used: HTML, CSS, SCSS, JavaScript, BEM methodology.',
-    img: '/miami4.png',
+    img: './miami4.png',
     link: 'https://olena-hapon.github.io/layout_miami/',
   },
   {
@@ -43,7 +43,7 @@ const items = [
     color: 'from-red-300 to-blue-300',
     title: '2048 game',
     desc: '2048 is a single-player sliding block puzzle game. The game’s objective is to slide numbered tiles on a grid to combine them to create a tile with the number 2048.',
-    img: '/game4.png',
+    img: './game4.png',
     link: 'https://olena-hapon.github.io/js_2048_game/',
   },
   {
@@ -51,7 +51,7 @@ const items = [
     color: 'from-red-300 to-blue-300',
     title: 'React Todo App',
     desc: 'A single-page application that allows users to create and manage a list of tasks. Technologies used: React, React Router, TypeScript, JavaScript, Sass (SCSS), BEM methodology.',
-    img: '/todo4.png',
+    img: './todo4.png',
     link: 'https://olena-hapon.github.io/react_todo-app/',
   }
 ]
